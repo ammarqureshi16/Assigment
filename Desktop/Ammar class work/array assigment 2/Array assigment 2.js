71b6e90cb518092b64eee8df389e0773b5314a25
ammarqureshi16 <ammarqureshi16>
2020-11-08 04:23:04 -0800

    abc

---
+++ Desktop/Ammar class work/array assigment 2/Array assigment 2.js
@@ -0,0 +1,107 @@
+ // question 1
+
+var array=[]
+array.push("Ammar")
+console.log(array)
+array.push("Qureshi")
+console.log(array)
+array.push("Ahmed")
+console.log(array)
+array.push("Ali")
+console.log(array)
+array.push("Saad")
+console.log(array)
+array.push("Khan")
+console.log(array)
+// question3
+
+var array=[]
+array.push("Ye to hoga")
+console.log(array)
+// question4
+
+var array=[]
+array.push(3)
+console.log(array)
+array.push(3)
+console.log(array)
+array.push(6)
+console.log(array)
+array.push(3)
+console.log(array)
+array.push(9)
+console.log(array)
+array.push(0)
+console.log(array)
+array.push(9)
+console.log(array)
+array.push(6)
+console.log(array)
+array.push(9)
+console.log(array)
+array.push(1)
+console.log(array)
+
+// question5
+// var array=[]
+// array.push(True)
+// console.log(array)
+// array.push(False)
+// console.log(array)
+
+// question7
+var array=[]
+array.push("Ufone")
+console.log(array)
+array.push("Zong")
+console.log(array)
+array.push("Jazz")
+console.log(array)
+array.push("Warid")
+console.log(array)
+// question8
+
+var array=[]
+array.push("Phd")
+array.push("M.Phil")
+array.push("Ms")
+array.push("Bcom")
+array.push("Bs")
+array.push("Bcs")
+array.push("Hsc")
+array.push("Ssc")
+console.log(array)
+
+// question9
+var array=[]
+array.push("Avengers : Age of Ultron")
+console.log(array)
+array.push("Spectre")
+console.log(array)
+array.push("Jurassic World")
+console.log(array)
+array.push("Inside Out")
+console.log(array)
+
+var car="Main,women"
+var res=car.split(",")
+console.log(res)
+// console.log(res.join())
+
+var vowels=["a","b","c","d","e","i","o","u","g","f","s"]
+
+for(let i=0;i<vowels.length;i++)
+    console.log(vowels[i])
+
+    if(vowels[i]==='a' ||vowels[i]==='e' ||vowels[i]==='i' ||vowels[i]==='o' ||vowels[i]==='u'){
+        console.log(vowels[i])
+   }
+
+var vowels=["a","b","c","d","e","i","o","u","g","f","s"]
+
+for(let i=0;i<vowels.length;i++)
+    console.log(vowels[i])
+
+    if(vowels[i]!='a' && vowels[i]!='e'  && vowels[i]!='i'  && vowels[i]!='o'  && vowels[i]!='u'){
+       console.log(vowels[i])
+    }
